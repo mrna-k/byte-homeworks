7f0db09ecf3e69555097ba7fceb570912f601e1e
MK <=>
2021-10-29 23:01:45 +0300

    add hw2

---
+++ homework2/script.js
@@ -0,0 +1,21 @@
+// task 1
+
+let userAge;
+let userName;
+let userSurname;
+
+// task 2
+
+let newUser;
+let shoppingList;
+let userOnline;
+let employeeSalary;
+let cursor;
+
+// task 3
+
+let hi = "Hello World!"
+console.log(hi);
+
+newUser = 'Marina';
+console.log('newUser', newUser);
